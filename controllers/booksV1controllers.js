d912b5f3aeb9a94bd449820ac151ee611c15f756
Antonio Alexandre Cordeiro dos Santos <antonioalexandre1984@gmail.com>
2022-08-13 21:16:31 -0300

    Finalização de CRUD

--- controllers/booksV1controllers.js
+++ controllers/booksV1controllers.js
@@ -1,9 +1,106 @@
 const {Book} = require('../models');
+const { Op } = require('sequelize');
 
 const booksV1Controllers = {
     getAllBooks: async (req, res,next) => {
-        const books = await Book.findAll();
-        return res.json(books);
+        try{
+            const books = await Book.findAll();
+            return res.status(200).json(books);
+        } catch (err) {
+            console.log(err)
+            if (err.name === 'SequelizeConnectionRefusedError') {
+                return res.status(500).json({ err: true, message: 'Internal server error' });
+            }   // 500 is the default status code for a server error
+            return res.status(400).json({err:true, message:"Requisição inválida"});
+        }
+    },
+    getOneBook: async (req, res, next) => {
+        try{
+            const {id} = req.params;
+            const book = await Book.findByPk(id);
+            if (!book) {
+                return res.status(404).json({ err: true, message: "Livro não encontrado" });
+            }
+            return res.status(200).json(book);
+        } catch (err) {
+            console.log(err)
+            if (err.name === 'SequelizeConnectionRefusedError') {
+                return res.status(500).json({ err: true, message: 'Internal server error' });
+            }   // 500 is the default status code for a server error
+            return res.status(400).json({err:true, message:"Requisição inválida"});
+        }
+    },
+    storeOneBook: async (req, res, next) => {
+        try{
+            const { title, total_pages, author, release_year,stock} = req.body;
+            const verifyBookExists = await Book.findOne({
+                where: {
+                    [Op.or]: [{ title},{author}]
+                }
+            });
+            if (verifyBookExists) {
+                return res.status(422).json({ err: true, message: "Livro já existe" });
+            }
+            const book = await Book.create({
+                title,
+                total_pages,
+                author,
+                release_year,
+                stock
+            });
+                return res.status(201).json(book);
+        } catch (err) {
+            console.log(err)
+            if (err.name === 'SequelizeConnectionRefusedError') {
+                return res.status(500).json({ err: true, message: 'Internal server error' });
+            }   // 500 is the default status code for a server error
+            return res.status(400).json({err:true, message:"Requisição inválida"});
+        }
+    },
+    updateOneBook: async (req, res, next) => {
+        try{
+            const { id } = req.params;
+            const { title, total_pages, author, release_year,stock} = req.body;
+            const verifyBookExists = await Book.findByPk(id);
+            if (!verifyBookExists) {
+                return res.status(404).json({ err: true, message: "Livro não encontrado" });
+            }
+            await Book.update({
+                title,
+                total_pages,
+                author,
+                release_year,
+                stock
+            },
+                { where: {id} }
+            );
+            const book = await Book.findByPk(id);
+            return res.status(200).json(book);
+        } catch (err) {
+            console.log(err)
+            if (err.name === 'SequelizeConnectionRefusedError') {
+                return res.status(500).json({ err: true, message: 'Internal server error' });
+            }   // 500 is the default status code for a server error
+            return res.status(400).json({err:true, message:"Requisição inválida"});
+        }
+    },
+    deleteOneBook: async (req, res, next) => {
+        try{
+            const { id } = req.params;
+            const book = await Book.findByPk(id);
+            if (!book) {
+                return res.status(404).json({ err: true, message: "Livro não encontrado" });
+            }
+            await book.destroy();
+            return res.status(204).json();
+        } catch (err) {
+            console.log(err)
+            if (err.name === 'SequelizeConnectionRefusedError') {
+                return res.status(500).json({ err: true, message: 'Internal server error' });
+            }   // 500 is the default status code for a server error
+            return res.status(400).json({err:true, message:"Requisição inválida"});
+        }
+
     }
 }
 
--- routes/booksV1.js
+++ routes/booksV1.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 const booksV1Controllers = require('../controllers/booksV1controllers');
 
-router.get('/',booksV1Controllers.getAllBooks);
+router.get("/", booksV1Controllers.getAllBooks);
+router.post("/",booksV1Controllers.storeOneBook);
+router.get("/:id", booksV1Controllers.getOneBook);
+router.put("/:id", booksV1Controllers.updateOneBook);
+router.delete("/:id", booksV1Controllers.deleteOneBook);
 
 module.exports = router;
